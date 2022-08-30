0f630a850cfbfd805edd319f1dc71e056f1ec600
Jeff <jbridges1119@gmail.com>
2022-08-30 14:53:40 -0600

    Add placeholder buttons

---
+++ src/components/LandingPage/Components/MainButtons.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+const MainButtons = () => {
+  return (
+    <div><Button>Projects</Button><Button>Resume</Button>  </div>
+  )
+}
+
+export default MainButtons
