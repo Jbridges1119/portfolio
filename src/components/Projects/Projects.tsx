396d7182902c91035f8ba0668f26cb730e2b325e
Jeff <jbridges1119@gmail.com>
2022-08-31 15:47:22 -0600

    Create boilerplate code

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,23 +1,17 @@
 import { Grid, Paper } from '@mui/material'
-import { Box, } from '@mui/system'
+import { Box } from '@mui/system'
 import React from 'react'
-import { Typography } from "@mui/material";
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
-const AboutMe = () => {
+
+const Projects = () => {
   return (
     <Box sx={{height: '800px',  background: `white`, position: 'relative', width:'100%',}}>
-      <Typography sx={{ color: "#81807c", fontSize: 20 }}>
-        <ArrowDownwardIcon sx={{ fontSize: 18 }} />
-        Scroll Down
-        <ArrowDownwardIcon sx={{ fontSize: 18 }} />
-      </Typography>
        <Grid
           container
           direction="row"
           justifyContent="center"
           alignItems="center"
         >
-          <Grid item md={10} xs={11}>
+          <Grid item xs={10}>
           <Grid
           container
           direction="row"
@@ -26,7 +20,7 @@ const AboutMe = () => {
         >
           
             <Grid item xs={5}>
-            <Paper elevation={8} sx={{width:'100%',height: '300px',background: `#b76bf0`, opacity: 0.15}}>hellow</Paper></Grid><Grid item xs={0.25}></Grid>
+            <Paper elevation={8} sx={{width:'100%',height: '300px',background: `#b76bf0`, opacity: 0.15}}>hellow</Paper></Grid><Grid item xs={0.5}></Grid>
             <Grid item xs={5}>
             <Paper elevation={8} sx={{width:'100%',height: '300px',background: `#379deb`, opacity: 0.15}}>hellow</Paper></Grid>
             <Grid item xs={5}>
@@ -40,4 +34,4 @@ const AboutMe = () => {
   )
 }
 
-export default AboutMe
+export default Projects
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <Box sx={{height: '800px', zIndex: 1, background: `white`, position: 'absolute', width:'100%',}}>Skills</Box>
+    <Box sx={{height: '800px', zIndex: 1, background: `white`, position: 'relative', width:'100%',}}>Skills</Box>
   )
 }
 
