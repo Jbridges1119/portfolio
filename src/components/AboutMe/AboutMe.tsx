8d3a0a172b5a096ac20706fcf4447d8427c0fe0a
Jeff <jbridges1119@gmail.com>
2022-08-31 14:30:41 -0600

    Create AboutMe section

---
+++ src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/system'
+import React from 'react'
+
+const AboutMe = () => {
+  return (
+    <Box sx={{height: '800px', zIndex: 10, background: `white`, position: 'relative', width:'100%',}}>About Me</Box>
+  )
+}
+
+export default AboutMe
