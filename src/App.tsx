948a1df36921b74cbb103ef39d7528aaccde848d
Jeff <jbridges1119@gmail.com>
2022-08-30 19:24:04 -0600

    import theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,23 @@ import React from 'react';
 import './styles/App.css';
 //Components
 import LandingPage from './components/LandingPage/LandingPage'
-
+//MUI
+import {
+  ThemeProvider,
+} from '@mui/material/styles';
+import theme from './styles/theme'
 
 
 function App() {
 
 
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
-      
       <LandingPage/>
-      Here we go
+      Scroll Down
     </div>
+    </ThemeProvider>
   );
 }
 
