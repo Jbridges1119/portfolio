3a5104fcb7e68619ad6d19859d514636253b3cd0
Jeff <jbridges1119@gmail.com>
2022-09-01 18:47:45 -0600

    Add footer and links

---
+++ src/sections/06_Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+//styles
+import '../../styles/06_FooterStyle/Footer.css'
+//Components
+import WavesBottom from "./components/WavesBottom";
+import FooterLinks from './components/FooterLinks'
+
+const Connect: React.FC = () => {
+  return (
+    <div className="hero-bottom" id="tsparticles1">
+    
+      <WavesBottom /> 
+      <FooterLinks/>
+    </div>
+  );
+};
+
+export default Connect;
