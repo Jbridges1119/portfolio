0aa3a1f5aa00e383c3e2b6a208e67cb5a6b5e148
Jeff <jbridges1119@gmail.com>
2022-09-01 18:50:35 -0600

    Create TechSkills placeholder

--- src/sections/03_Skills/Skills.tsx
+++ src/sections/03_Skills/Skills.tsx
@@ -1,10 +1,37 @@
-import { Box } from '@mui/system'
-import React from 'react'
+import { Grid } from '@mui/material'
+import { Box, } from '@mui/system'
 
-const Skills = () => {
+import TechSkills from './components/TechSkills'
+const AboutMe = () => {
   return (
-    <Box sx={{height: '800px', zIndex: 1, background: `white`, position: 'relative', width:'100%',}}>Skills</Box>
+    <Box sx={{  background: `white`, position: 'relative', width:'100%',}}>
+      <Box height={'15vh'}></Box>
+       <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item md={10} sm={10} xs={10}>
+          <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          
+            <Grid item xs={12} sm={12} md={5}>
+             <TechSkills/>
+            </Grid><Grid item xs={0.25} lg={0.75}></Grid>
+            <Grid item xs={12} sm={12} md={5}>
+              Experience
+            </Grid>
+          
+            </Grid>
+            </Grid></Grid>
+    
+    </Box>
   )
 }
 
-export default Skills
+export default AboutMe
