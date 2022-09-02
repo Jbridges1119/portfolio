b4924d8c1bc447bfa1420b1e02c644d28b8712b8
Jeff <jbridges1119@gmail.com>
2022-09-01 18:50:57 -0600

    Add footer wave design

---
+++ src/sections/06_Footer/components/WavesBottom.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const WavesBottom: React.FC = () => {
+  return (
+    <>
+      <div className="w-bottom wave-bottom"></div>
+      <div className="w-bottom wave2-bottom"></div>
+      <div className="w-bottom wave3-bottom"></div>
+      <div className="w-bottom wave4-bottom"></div>
+    </>
+  );
+};
+
+export default WavesBottom;
