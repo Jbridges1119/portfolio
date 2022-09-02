0a3a9c3c196f138c8b26b87217e4fd63ba6a5eaf
Jeff <jbridges1119@gmail.com>
2022-09-02 03:03:09 -0600

    Add color to hello

--- src/sections/02_AboutMe/components/Story.tsx
+++ src/sections/02_AboutMe/components/Story.tsx
@@ -5,7 +5,7 @@ const Story = () => {
   return (
     
       <Stack spacing={2}>
-            <Typography variant="h4" sx={{ color: "black", textAlign:'left'}}>Hello!</Typography>
+            <Typography variant="h4" sx={{ color: "#4345E8", textAlign:'left'}}>Hello!</Typography>
             <Divider />
             <Typography variant="body1"sx={{ color: "black", textAlign:'left'}}>My Name is Jeff Bridges and I'm a passionate Full-Stack Developer. I pivoted from being a Certified HVAC/R Technician due to always having an interest in problem solving and bringing ideas to life. With Web Development, I can nurture my creativity through a new medium and I'm excited to build intuitive, responsive, and efficient web applications. I graduated from Lighthouse Labs Bootcamp in August 2022 and am looking to build upon the experiences I gained there. Currently I'm learning Typescript and refining the skills already learned. </Typography>
             <Typography variant="body1"sx={{ color: "black", textAlign:'left'}}>
