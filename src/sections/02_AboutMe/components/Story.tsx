f2b099d5692270b96c406d327f1192dc52f08b42
Jeff <jbridges1119@gmail.com>
2022-09-01 18:48:56 -0600

    Add text divider

--- src/sections/02_AboutMe/components/Story.tsx
+++ src/sections/02_AboutMe/components/Story.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from '@mui/material'
+import { Stack, Typography,Divider } from '@mui/material'
 import React from 'react'
 
 const Story = () => {
@@ -6,9 +6,11 @@ const Story = () => {
     
       <Stack spacing={2}>
             <Typography variant="h4" sx={{ color: "black", textAlign:'left'}}>Hello!</Typography>
+            <Divider />
             <Typography variant="body1"sx={{ color: "black", textAlign:'left'}}>My Name is Jeff Bridges and I'm a passionate Full-Stack Developer. I pivoted from being a Certified HVAC/R Technician due to always having an interest in problem solving and bringing ideas to life. With Web Development, I can nurture my creativity through a new medium and I'm excited to build intuitive, responsive, and efficient web applications. I graduated from Lighthouse Labs Bootcamp in August 2022 and am looking to build upon the experiences I gained there. Currently I'm learning Typescript and refining the skills already learned. </Typography>
             <Typography variant="body1"sx={{ color: "black", textAlign:'left'}}>
             I'd love to hear from you. Whether it's a job opportunity, a project collaboration, or just to chat. Feel free to contact me.</Typography>
+            <Divider />
             <Typography variant="body1" sx={{ color: "black", textAlign:'left', py:3}}><i>~ Don't Try to Be the Best. Just Try To Be 1% Better Everyday.</i></Typography>
             </Stack>   
   )
