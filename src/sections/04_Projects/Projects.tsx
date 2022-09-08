3c4d7b8d776f007db8d8874869f0309e3f0abb1e
Jeff <jbridges1119@gmail.com>
2022-09-08 15:27:53 -0600

    Add hover effect and removed commented code

--- src/sections/04_Projects/Projects.tsx
+++ src/sections/04_Projects/Projects.tsx
@@ -10,7 +10,7 @@ import {
 } from "@mui/material";
 import { Box } from "@mui/system";
 //Style
-import { motion } from "framer-motion";
+import { motion, useInView, useScroll, useTransform } from "framer-motion";
 import "../../styles/04_ProjectsStyle/Projects.css";
 //Components
 import ProjectsCard from "./components/ProjectsCard";
@@ -28,44 +28,50 @@ import { usePageLocation } from "../../hooks/pageContext";
 
 const Projects = () => {
   const { currentPercent, projectSect } = usePageLocation();
+  const { scrollYProgress, scrollY } = useScroll();
+  const yValue = useTransform(
+    scrollYProgress,
+    [0, 0.5, 1],
+    [50, 70, 100 * -0.35]
+  );
+  const opacity = useTransform(scrollYProgress, [0.48, 0.54], [0, 1]);
   return (
     <>
-    
-  
-    <Box ref={projectSect}
-      sx={{
-        height: "100%",
-        background: `white`,
-        position: "relative",
-        zIndex: 1,
-        width: "100%",
-      }}
-    >
-   <Box height={"4vh"} />
-   
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
+      <Box
+        ref={projectSect}
+        sx={{
+          height: "100%",
+          background: `white`,
+          position: "relative",
+          zIndex: 1,
+          width: "100%",
+        }}
       >
-        
-        <Grid
-          item
-          lg={8}
-          md={8}
-          sm={10}
-          xs={10}
-          display={"grid"}
-          //If I make another project add tinyapp and set this to xl:'repeat(3, 1fr)
-          gridTemplateColumns={{  md:"repeat(2, 1fr)", xs: "1fr" }}
-          
-          gap={6}
-        >
-          <ProjectsCard />
-        </Grid>
-      </Grid><Box height={"20vh"} />
-    </Box>
+        <Box height={"4vh"} />
+        {/* <motion.div style={{ opacity }}> */}
+          <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid
+              item
+              lg={8}
+              md={8}
+              sm={10}
+              xs={10}
+              display={"grid"}
+              //If I make another project add tinyapp and set this to xl:'repeat(3, 1fr)
+              gridTemplateColumns={{ md: "repeat(2, 1fr)", xs: "1fr" }}
+              gap={6}
+            >
+              <ProjectsCard />
+            </Grid>
+          </Grid>
+        {/* </motion.div> */}
+        <Box height={"15vh"} />
+      </Box>
     </>
   );
 };
--- src/sections/04_Projects/components/ProjectsCard.tsx
+++ src/sections/04_Projects/components/ProjectsCard.tsx
@@ -6,114 +6,69 @@ import {
   Paper,
   Stack,
   Typography,
+  Box,
 } from "@mui/material";
-import { Box } from "@mui/system";
-//Style
-import { motion, useInView } from "framer-motion";
 //Helper
 import { projectsInfo } from "../../../helpers/ProjectsHelper";
+//Components
+import ButtonMap from './ButtonMap'
+import IconMap from './IconMap'
 
-const ProjectsCard = () => {
- 
-
-  // const imageAnimate = {
-  //   offScreen: {  scale: 0.85, opacity: 1 },
-  //   onScreen: {
-
-  //     scale: 0.85,
-  //     opacity: 1,
-      
-  //     transition: { type: "tween", bounce: 0.4, duration: 1 },
-  //   },
-  // };
-  // const textAnimate = {
-  //   offScreen: { y: 100,  opacity: 0 },
-  //   onScreen: {
-  //     y: 0,
-  //     opacity: 1,
-  //     // scale: [0.85, 1.1, 1],
-  //     transition: { type: "spring", bounce: 0.4, duration: 1.5 },
-  //   },
-  // };
-  // const buttonAnimate = {
-  //   offScreen: {opacity: 0, scale: 0},
-  //   onScreen: {
-  //     opacity: 1,
-  //     scale:  1,
-  //     transition: { type: "spring", bounce: 0.4, duration: 1.5 },
-  //   },
-  // }
-  const cardAnimation ={
-    offScreen: {y:50, opacity: 0},
-    onScreen: {
-      y:0,
-      opacity: 1,
-      transition: {type: 'tween', duration: 0.25}
-    }
-  }
-  const projectCard = projectsInfo.map((project) => {
-    type buttonType = {
-      name: string;
-      link: string;
-    };
-    
-    const buttonLinks = project.buttons.map((button: buttonType) => {
-      
-      return (
-        <Button
-          key={button.link}
-          href={button.link}
-          target="_blank"
-          sx={{
-            display: "flex",
-            color: "#fff",
-            border: "2px solid white",
-            borderRadius: 25,
-            fontSize: 14,
-            backgroundColor: "#a557ff",
-            width: "100px",
-            pt: 1.2,
-            fontWeight: "bold",
-          }}
-        >
-          {button.name}
-        </Button>
-      );
-    });
-
+const ProjectsCard: React.FC = () => {
+  return(
+     <>{projectsInfo.map((project) => {
     return (
-      <motion.div className="fill" key={project.name}
-   initial={'offScreen'}
-   whileInView={'onScreen'}
-   viewport={{once:true}}
-   transition={{staggerChildren: 0.5}}>
-    <motion.div className="fill" variants={cardAnimation}>
       <Paper
-        
+        key={project.name}
         elevation={24}
         sx={{
           width: "100%",
           background: `#E3EAFD`,
           borderRadius: 5,
-          display: 'flex',
-          flexDirection:'column',
-          alignItems:'center'
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
-      
-          <Paper  sx={{ borderRadius: 5,m:2, width:'90%' }}>
-            <CardMedia
-              component="img"
-              image={project.gif}
-              alt=""
-              sx={{
-                objectFit:'contain',
-                
-                borderRadius: 5,position: 'relative'
-              }}
-            />
-          </Paper>
-     
+        <Box
+          className="container"
+          sx={{ borderRadius: 10, m: 2, width: "90%", position: "relative" }}
+        >
+          <CardMedia
+            className="project-gif"
+            component="img"
+            image={project.gif}
+            alt=""
+          />
+          <Box className="wrapper-info">
+            <Typography variant="h3" color={"white"} fontWeight={"bold"} mt={1}>
+              Built With
+            </Typography>
+            <Box className="center-all">
+             <IconMap stack={project.stack}/>
+            </Box>
+            <div>
+              <Button
+                href={project.buttons[0].link}
+                target={"_blank"}
+                sx={{
+                  color: "#fff",
+                  borderRadius: 25,
+                  fontSize: 14,
+                  border: "3px solid white",
+                  my: 1,
+                  pt: 0.9,
+                  textTransform:'none'
+                }}
+              >
+                <Typography variant="h5" color={"white"} px={2}>
+                  More Info
+                </Typography>
+              </Button>
+            </div>
+          </Box>
+        </Box>
+
         <Grid
           container
           direction="row"
@@ -122,41 +77,26 @@ const ProjectsCard = () => {
         >
           <Grid item xs={11}>
             <Stack spacing={0}>
-              <motion.div
-                className="fill"
-              
-                // variants={textAnimate}
-              >
-                <Typography variant="h4"  color={"#4345E8"}>
-                  {project.name}
-                </Typography>
-              </motion.div>
-              <motion.div
-                className="fill"
-               
-                // variants={textAnimate}
-              >
-              <Typography variant="body1" >{project.description}</Typography></motion.div>
-              <motion.div
-                className="fill"
-               
-                // variants={buttonAnimate}
-              >
+              <Typography variant="h4" color={"#4345E8"}>
+                {project.name}
+              </Typography>
+              <Typography variant="body1">{project.description}</Typography>
               <Box
                 display={"flex"}
                 justifyContent={"space-around"}
-                sx={{ py: 1.5 }}
+                py={1.5}
               >
-                {buttonLinks}
-              </Box></motion.div>
+                <ButtonMap buttons={project.buttons}/>
+              </Box>
             </Stack>
           </Grid>
         </Grid>
-      </Paper></motion.div></motion.div>
+      </Paper>
     );
-  });
-
-  return <>{projectCard}</>;
+  })}
+  </>
+  
+  )
 };
 
 export default ProjectsCard;
