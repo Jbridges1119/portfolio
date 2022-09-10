d1307956c54bce89bfd9f90b552554937f350886
Jeff <jbridges1119@gmail.com>
2022-09-09 20:56:57 -0600

    Minor desc update

--- src/helpers/ProjectsHelper.tsx
+++ src/helpers/ProjectsHelper.tsx
@@ -87,7 +87,7 @@ everyone is prepared to maximize their time together.`,
   {
     name: "Tweeter",
     description:
-      "Tweeter is a simple, single-page social media platform where users can leave 140 character posts. Users can also read through their list of tweets in chronological order.",
+      "Tweeter is a single-page social media platform where users can leave 140 character posts. Users can also read through their list of tweets in chronological order.",
     gif: tweeter,
     buttons: [
       {
